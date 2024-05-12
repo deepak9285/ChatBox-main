d124a4e4416ad4379c47f9c4c641033df66af2a6
deepak9285 <83562221+deepak9285@users.noreply.github.com>
2024-05-12 17:58:37 +0530

    first commit

--- frontend/src/components/HomeComponents/Footer/Footer.js
+++ frontend/src/components/HomeComponents/Footer/Footer.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import './Footer.css'; // Assuming you save your CSS in a file named Footer.css
+import React from "react";
+import "./Footer.css"; // Assuming you save your CSS in a file named Footer.css
 
 const Footer = () => {
   return (
@@ -9,31 +9,53 @@ const Footer = () => {
           <div className="footer-col">
             <h4>About</h4>
             <ul>
-              <li><a href="#">About Us</a></li>
-              <li><a href="#">Contact Us</a></li>
-              <li><a href="#">Privacy Policy</a></li>
-              <li><a href="#">Terms of Service</a></li>
+              <li>
+                <a href="#">About Us</a>
+              </li>
+              <li>
+                <a href="#">Contact Us</a>
+              </li>
+              <li>
+                <a href="#">Privacy Policy</a>
+              </li>
+              <li>
+                <a href="#">Terms of Service</a>
+              </li>
             </ul>
           </div>
           <div className="footer-col">
             <h4>Help</h4>
             <ul>
-              <li><a href="#">FAQ</a></li>
-              <li><a href="#">Support</a></li>
+              <li>
+                <a href="#">FAQ</a>
+              </li>
+              <li>
+                <a href="#">Support</a>
+              </li>
             </ul>
           </div>
           <div className="footer-col">
             <h4>Connect</h4>
             <ul>
-              <li><a href="#">Facebook</a></li>
-              <li><a href="#">Twitter</a></li>
-              <li><a href="#">Instagram</a></li>
-              <li><a href="#">LinkedIn</a></li>
+              <li>
+                <a href="#">Facebook</a>
+              </li>
+              <li>
+                <a href="#">Twitter</a>
+              </li>
+              <li>
+                <a href="#">Instagram</a>
+              </li>
+              <li>
+                <a href="#">LinkedIn</a>
+              </li>
             </ul>
           </div>
           <div className="footer-col">
             <h4>ChatApp</h4>
-            <p>Connect with us on out ChatBox for real-time support and updates.</p>
+            <p>
+              Connect with us on our ChatBox for real-time support and updates.
+            </p>
             <p>Email: support@chatbox.com</p>
           </div>
         </div>
@@ -43,6 +65,6 @@ const Footer = () => {
       </p>
     </footer>
   );
-}
+};
 
 export default Footer;
